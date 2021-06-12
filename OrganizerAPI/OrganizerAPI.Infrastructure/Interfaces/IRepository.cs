359695ff8f40549397a363613401a80f38d45c9a
Andrii <sytnyk.andrii@gmail.com>
2021-06-12 21:54:56 +0300

    IRepository interface was created.

---
+++ OrganizerAPI/OrganizerAPI.Infrastructure/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using OrganizerAPI.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizerAPI.Infrastructure.Interfaces
+{
+    public interface IRepository<T> where T : Entity
+    {
+        Task<T> GetById(int id);
+
+        Task<IReadOnlyList<T>> GetList();
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
