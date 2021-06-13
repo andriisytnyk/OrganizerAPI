c2e99d2b7d5f7487becc47d4afe63e40a2007417
Andrii <sytnyk.andrii@gmail.com>
2021-06-13 18:15:24 +0300

    TaskService service was created.

---
+++ OrganizerAPI/OrganizerAPI.Domain/Services/TaskService.cs
@@ -0,0 +1,50 @@
+﻿using OrganizerAPI.Domain.Interfaces;
+using OrganizerAPI.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizerAPI.Domain.Services
+{
+    public class TaskService : ITaskService
+    {
+        private readonly TaskRepository repository;
+
+        public TaskService(TaskRepository taskRepository)
+        {
+
+        }
+
+        public int Create(Models.Models.Task entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Models.Models.Task entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Models.Models.Task> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.Models.Task GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Models.Models.Task entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
