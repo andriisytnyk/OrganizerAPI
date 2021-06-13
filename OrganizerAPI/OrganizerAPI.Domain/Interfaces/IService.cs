e8819e6a012061d381b1b057a23bbd32327a1a47
Andrii <sytnyk.andrii@gmail.com>
2021-06-13 18:14:53 +0300

    IService interface was created.

---
+++ OrganizerAPI/OrganizerAPI.Domain/Interfaces/IService.cs
@@ -0,0 +1,20 @@
+﻿using OrganizerAPI.Models.Models;
+using System.Collections.Generic;
+
+namespace OrganizerAPI.Domain.Interfaces
+{
+    public interface IService<T> where T : Entity
+    {
+        List<T> GetAll();
+
+        T GetById(int id);
+
+        int Create(T entity);
+
+        void Update(int id, T entity);
+
+        void Delete(T entity);
+
+        void DeleteById(int id);
+    }
+}
