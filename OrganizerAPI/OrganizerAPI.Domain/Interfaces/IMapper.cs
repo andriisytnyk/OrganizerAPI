5232759d11cd1739367b2688edc753fac5e2050f
Andrii <sytnyk.andrii@gmail.com>
2021-06-14 22:43:20 +0300

    Mapper was created.

---
+++ OrganizerAPI/OrganizerAPI.Domain/Interfaces/IMapper.cs
@@ -0,0 +1,16 @@
+﻿using OrganizerAPI.Models.Models;
+using OrganizerAPI.Shared.ModelsDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizerAPI.Domain.Interfaces
+{
+    public interface IMapper
+    {
+        UserTaskDTO MapUserTask(UserTask value);
+        UserTask MapUserTask(UserTaskDTO value);
+    }
+}
---
+++ OrganizerAPI/OrganizerAPI.Domain/Mapping/Mapper.cs
@@ -0,0 +1,38 @@
+﻿using OrganizerAPI.Domain.Interfaces;
+using OrganizerAPI.Models.Models;
+using OrganizerAPI.Shared.ModelsDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizerAPI.Domain.Mapping
+{
+    public class Mapper : IMapper
+    {
+        public UserTaskDTO MapUserTask(UserTask value)
+        {
+            return new UserTaskDTO
+            {
+                Id = value.Id,
+                Title = value.Title,
+                Description = value.Description,
+                Date = value.Date,
+                Status = value.Status
+            };
+        }
+
+        public UserTask MapUserTask(UserTaskDTO value)
+        {
+            return new UserTask
+            {
+                Id = value.Id,
+                Title = value.Title,
+                Description = value.Description,
+                Date = value.Date,
+                Status = value.Status
+            };
+        }
+    }
+}
