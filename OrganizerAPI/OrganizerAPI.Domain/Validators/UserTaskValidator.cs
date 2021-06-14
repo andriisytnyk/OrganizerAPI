90cb6fa04926683e77a3df65e49f521c160c4d6d
Andrii <sytnyk.andrii@gmail.com>
2021-06-14 22:43:47 +0300

    Validator for UserTaskDTO class was created.

---
+++ OrganizerAPI/OrganizerAPI.Domain/Validators/UserTaskValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using OrganizerAPI.Models.Common;
+using OrganizerAPI.Shared.ModelsDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizerAPI.Domain.Validators
+{
+    public class UserTaskValidator : AbstractValidator<UserTaskDTO>
+    {
+        public UserTaskValidator()
+        {
+            RuleFor(g => g.Title).NotEmpty().Length(1, 50);
+            RuleFor(g => g.Description).Length(1, 200);
+            RuleFor(g => g.Status).IsInEnum();
+            RuleFor(g => g.Date).NotNull();
+        }
+    }
+}
